7df6e65dac504be03728c511be466d6e48765534
KrDior <xzx89s@gmail.com>
2019-03-11 01:08:16 +0300

    first commit

--- src/node.js
+++ src/node.js
@@ -1,22 +1,54 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.right = null;
+		this.left = null;
 	}
 
 	appendChild(node) {
-
+		if (this.left == null) {
+			this.left = node;
+			node.parent = this;
+		} else if (this.left !== null && this.right == null) {
+			this.right = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if (this.left == node) {
+			this.left = null;
+			node.parent = null;
+		} else if (this.right == node) {
+			this.right = null;
+			node.parent = null;
+		} else {
+			throw new Error;
+		}
 	}
 
 	remove() {
+		if (this.parent == null) {
+			return;
+		} else {
+			this.parent.removeChild(this);
+		}
 
 	}
 
 	swapWithParent() {
-		
+		if (this.parent == null) {
+			return;
+		} else if (this.parent.parent == null) {
+			this.parent.parent = this;
+		} else if (this.parent.parent !== null && this.parent.parent.parent == null){
+			let elem = this.parent.parent;
+	
+			this.parent.parent = this;
+			this.parent = elem;
+		} 
 	}
 }
 
--- src/queue.js
+++ src/queue.js
@@ -2,11 +2,16 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
+		this.items = [];
+		this.max = maxSize - 1;
 
+		while (maxSize > 0) {
+			this.queue.push(null);
+			maxSize--;
+		}
 	}
-
 	push(data, priority) {
-
+		let qItem = new Node(data, priority);
 	}
 
 	shift() {
@@ -14,11 +19,13 @@ class PriorityQueue {
 	}
 
 	size() {
-
+		return this.items.length;
 	}
 
 	isEmpty() {
-		
+		if (this.items.length == 0) {
+			return true;
+		};
 	}
 }
 
